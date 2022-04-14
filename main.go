2e046ccaf82cd0ef1412cb11cdbc2eb4cd27d6cc
Vyacheslav Artemyev <viartemev@Vyacheslavs-MacBook-Pro.local>
2022-04-14 22:28:46 +0300

    refactoring

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	flag "github.com/spf13/pflag"
 	tele "gopkg.in/telebot.v3"
 	"log"
-	"nhl-recap/client"
-	"nhl-recap/domain"
-	"strings"
-	"sync"
+	"nhl-recap/nhl"
 	"time"
 )
 
@@ -45,67 +41,9 @@ func init() {
 
 func main() {
 	bot.Settings.Handle("/games", func(c tele.Context) error {
-		return c.Send(fetchGames(), &tele.SendOptions{ParseMode: tele.ModeMarkdown})
+		return c.Send(nhl.FetchGames(), &tele.SendOptions{ParseMode: tele.ModeMarkdown})
 	})
 
-	fmt.Println("Telegran bot NHL Recap starting...")
+	fmt.Println("Telegram bot NHL Recap starting...")
 	bot.Settings.Start()
-}
-
-type GameInfo struct {
-	Title string
-	Video string
-}
-
-func fetchGames() string {
-	var wg sync.WaitGroup
-	gamesVideos := make(map[int]*GameInfo)
-	schedule := client.HttpGet[domain.Schedule]("https://statsapi.web.nhl.com/api/v1/schedule")
-	finishedGames := filterFinishedGames(schedule)
-	if len(finishedGames) == 0 {
-		return "There are no finished games"
-	}
-	for _, games := range finishedGames {
-		wg.Add(1)
-		go func(games domain.Games) {
-			gameInfo := client.HttpGet[domain.Game]("https://statsapi.web.nhl.com/api/v1/game/" + fmt.Sprintf("%v", games.GamePk) + "/content")
-			video := extractGameVideo(gameInfo)
-			title := fmt.Sprintf("*%s*\n🥅🏒 %v - %v ", games.Teams.TeamsAndWinner(), games.Teams.Home.Score, games.Teams.Away.Score)
-			gamesVideos[games.GamePk] = &GameInfo{title, video}
-			defer wg.Done()
-		}(games)
-	}
-	wg.Wait()
-
-	var buffer bytes.Buffer
-	for _, info := range gamesVideos {
-		buffer.WriteString(fmt.Sprintf("%v[Recap](%v)\n", info.Title, info.Video))
-	}
-	return buffer.String()
-}
-
-func filterFinishedGames(schedule domain.Schedule) (finishedGames []domain.Games) {
-	for _, date := range schedule.Dates {
-		for _, game := range date.Games {
-			if game.Status.AbstractGameState == "Final" {
-				finishedGames = append(finishedGames, game)
-			}
-		}
-	}
-	return
-}
-
-func extractGameVideo(game domain.Game) (video string) {
-	for _, media := range game.Media.Epg {
-		if media.Title == "Recap" {
-			for _, item := range media.Items {
-				for _, playback := range item.Playbacks {
-					if strings.Contains(playback.Name, "FLASH_1800K") {
-						video = playback.Url
-					}
-				}
-			}
-		}
-	}
-	return
 }
---
+++ nhl/nhl.go
@@ -0,0 +1,68 @@
+package nhl
+
+import (
+	"bytes"
+	"fmt"
+	"nhl-recap/client"
+	domain2 "nhl-recap/nhl/domain"
+	"strings"
+	"sync"
+)
+
+func extractGameVideo(game domain2.Game) (video string) {
+	for _, media := range game.Media.Epg {
+		if media.Title == "Recap" {
+			for _, item := range media.Items {
+				for _, playback := range item.Playbacks {
+					if strings.Contains(playback.Name, "FLASH_1800K") {
+						video = playback.Url
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+type GameInfo struct {
+	Title string
+	Video string
+}
+
+func FetchGames() string {
+	var wg sync.WaitGroup
+	gamesVideos := make(map[int]*GameInfo)
+	schedule := client.HttpGet[domain2.Schedule]("https://statsapi.web.nhl.com/api/v1/schedule")
+	finishedGames := filterFinishedGames(schedule)
+	if len(finishedGames) == 0 {
+		return "There are no finished games"
+	}
+	for _, games := range finishedGames {
+		wg.Add(1)
+		go func(games domain2.Games) {
+			gameInfo := client.HttpGet[domain2.Game]("https://statsapi.web.nhl.com/api/v1/game/" + fmt.Sprintf("%v", games.GamePk) + "/content")
+			video := extractGameVideo(gameInfo)
+			title := fmt.Sprintf("*%s*\n🥅🏒 %v - %v ", games.Teams.TeamsAndWinner(), games.Teams.Home.Score, games.Teams.Away.Score)
+			gamesVideos[games.GamePk] = &GameInfo{title, video}
+			defer wg.Done()
+		}(games)
+	}
+	wg.Wait()
+
+	var buffer bytes.Buffer
+	for _, info := range gamesVideos {
+		buffer.WriteString(fmt.Sprintf("%v[Recap](%v)\n", info.Title, info.Video))
+	}
+	return buffer.String()
+}
+
+func filterFinishedGames(schedule domain2.Schedule) (finishedGames []domain2.Games) {
+	for _, date := range schedule.Dates {
+		for _, game := range date.Games {
+			if game.Status.AbstractGameState == "Final" {
+				finishedGames = append(finishedGames, game)
+			}
+		}
+	}
+	return
+}
