c303c5e97783568a9943305b81282760a6bd200a
Vyacheslav Artemyev <viartemev@Vyacheslavs-MacBook-Pro.local>
2022-05-04 16:26:13 +0300

    Add unique set & unsubscribe

---
+++ util/set.go
@@ -0,0 +1,25 @@
+package util
+
+type Set[K comparable] struct {
+	values map[K]bool
+}
+
+func NewSet[K comparable]() *Set[K] {
+	return &Set[K]{
+		values: make(map[K]bool),
+	}
+}
+
+func (s *Set[K]) Add(key K) {
+	s.values[key] = true
+}
+
+func (s *Set[K]) Delete(key K) {
+	delete(s.values, key)
+}
+
+func (s *Set[K]) Range(fun func(value K)) {
+	for key, _ := range s.values {
+		fun(key)
+	}
+}
