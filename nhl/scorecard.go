43414ec98d7cce49a53c78e699d49d68418a247c
Vyacheslav Artemyev <vyacheslav.artemyev@sbermarket.ru>
2022-12-05 18:16:38 +0700

    Prettify output

--- nhl/scorecard.go
+++ nhl/scorecard.go
@@ -1,48 +1,48 @@
 package nhl
 
 import (
-    "bytes"
-    "fmt"
-    "github.com/golang/freetype"
-    "github.com/golang/freetype/truetype"
-    log "github.com/sirupsen/logrus"
-    "golang.org/x/image/draw"
-    "golang.org/x/image/font"
-    "golang.org/x/image/font/gofont/goregular"
-    "golang.org/x/image/math/fixed"
-    "image"
-    "image/color"
-    "image/png"
+	"bytes"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"strconv"
+
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/image/draw"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
+	"golang.org/x/image/math/fixed"
 )
-
-
 
 func GenerateScoreCard(message *GameInfo) []byte {
 	var width = 300
 	var height = 100
-	img, _ := createCard(width, height, "#ffffff", fmt.Sprintf("%d - %d", message.HomeTeam.Score, message.AwayTeam.Score))
+	img, _ := createCard(width, height, "#ffffff", message)
 	buf := new(bytes.Buffer)
 	_ = png.Encode(buf, img)
 	return buf.Bytes()
 }
 
-func createCard(width int, height int, bg string, text string) (*image.RGBA, error) {
+func createCard(width int, height int, bg string, message *GameInfo) (*image.RGBA, error) {
 	bgColor, err := hexToRGBA(bg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	background := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(background, background.Bounds(), &image.Uniform{C: bgColor}, image.Point{}, draw.Src)
-	_ = drawText(background, text)
+	_ = drawText(background, message)
 	return background, err
 }
 
-func drawText(canvas *image.RGBA, text string) error {
+func drawText(canvas *image.RGBA, message *GameInfo) error {
 	var (
 		fgColor  image.Image
 		fontFace *truetype.Font
 		err      error
-		fontSize = 32.0
+		fontSize = 25.0
 	)
 	fgColor = image.Black
 	fontFace, err = freetype.ParseFont(goregular.TTF)
@@ -54,15 +54,43 @@ func drawText(canvas *image.RGBA, text string) error {
 			Hinting: font.HintingFull,
 		}),
 	}
-	textBounds, _ := fontDrawer.BoundString(text)
-	xPosition := (fixed.I(canvas.Rect.Max.X) - fontDrawer.MeasureString(text)) / 2
-	textHeight := textBounds.Max.Y - textBounds.Min.Y
-	yPosition := fixed.I((canvas.Rect.Max.Y)-textHeight.Ceil())/2 + fixed.I(textHeight.Ceil())
+
+	// Home team
+	homeTeamTextBound, _ := fontDrawer.BoundString(message.HomeTeam.Name)
+	homeTeamXPosition := fixed.I(90)
+	homeTeamTextHeight := homeTeamTextBound.Max.Y - homeTeamTextBound.Min.Y
+	homeTeamYPosition := fixed.I((canvas.Rect.Max.Y)-homeTeamTextHeight.Ceil())/4 + fixed.I(homeTeamTextHeight.Ceil())
 	fontDrawer.Dot = fixed.Point26_6{
-		X: xPosition,
-		Y: yPosition,
+		X: homeTeamXPosition,
+		Y: homeTeamYPosition,
 	}
-	fontDrawer.DrawString(text)
+	fontDrawer.DrawString(message.HomeTeam.Name)
+
+	homeTeamXPosition = fixed.I(canvas.Rect.Max.X - 50)
+	fontDrawer.Dot = fixed.Point26_6{
+		X: homeTeamXPosition,
+		Y: homeTeamYPosition,
+	}
+	fontDrawer.DrawString(strconv.Itoa(message.HomeTeam.Score))
+
+	// Away team
+	awayTeamTextBound, _ := fontDrawer.BoundString(message.AwayTeam.Name)
+	awayTeamXPosition := fixed.I(90)
+	awayTeamTextHeight := awayTeamTextBound.Max.Y - awayTeamTextBound.Min.Y
+	awayTeamYPosition := fixed.I(canvas.Rect.Max.Y) - fixed.I(awayTeamTextHeight.Ceil())
+	fontDrawer.Dot = fixed.Point26_6{
+		X: awayTeamXPosition,
+		Y: awayTeamYPosition,
+	}
+	fontDrawer.DrawString(message.AwayTeam.Name)
+
+	awayTeamXPosition = fixed.I(canvas.Rect.Max.X - 50)
+	fontDrawer.Dot = fixed.Point26_6{
+		X: awayTeamXPosition,
+		Y: awayTeamYPosition,
+	}
+	fontDrawer.DrawString(strconv.Itoa(message.AwayTeam.Score))
+
 	return err
 }
 
--- nhl/scorecard_test.go
+++ nhl/scorecard_test.go
@@ -1,47 +1,47 @@
 package nhl
 
 import (
-    "bytes"
-    log "github.com/sirupsen/logrus"
-    "image"
-    "image/png"
-    "os"
-    "testing"
-    "time"
+	"bytes"
+	"image"
+	"image/png"
+	"os"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func TestGamesUnmarshalling(t *testing.T) {
 	game := &GameInfo{
-		GamePk:   0,
-		Video:    "Hello",
+		GamePk: 0,
+		Video:  "link",
 		HomeTeam: &TeamInfo{
-            Name:  "Home Team",
-            Score: 1,
-        },
+			Name:  "COL",
+			Score: 7,
+		},
 		AwayTeam: &TeamInfo{
-            Name:  "Away Team",
-            Score: 3,
-        },
+			Name:  "PHI",
+			Score: 1,
+		},
 	}
-    scoreCard := GenerateScoreCard(game)
-    save(scoreCard)
+	scoreCard := GenerateScoreCard(game)
+	save(scoreCard)
 }
 
 func save(imgBytes []byte) {
-    img, _, err1 := image.Decode(bytes.NewReader(imgBytes))
-    if err1 != nil {
-        log.Fatal(err1)
-    }
-    out, err2 := os.Create(time.Now().Format("2006-01-02 15:04:05.000000") + ".png")
-    defer func(out *os.File) {
-        err := out.Close()
-        if err != nil {
-            log.Fatal(err2)
-        }
-    }(out)
+	img, _, err1 := image.Decode(bytes.NewReader(imgBytes))
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	out, err2 := os.Create("scorecard.png")
+	defer func(out *os.File) {
+		err := out.Close()
+		if err != nil {
+			log.Fatal(err2)
+		}
+	}(out)
 
-    err3 := png.Encode(out, img)
-    if err3 != nil {
-        log.Fatal(err3)
-    }
+	err3 := png.Encode(out, img)
+	if err3 != nil {
+		log.Fatal(err3)
+	}
 }
